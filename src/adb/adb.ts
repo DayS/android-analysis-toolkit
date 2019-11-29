7bf82a2814394f52446b3787b437ffda0084b3d0
Damien Villeneuve <contact@dvilleneuve.fr>
2019-11-29 18:27:51 +0100

    Fix warnings on adb.ts file

--- src/adb/adb.ts
+++ src/adb/adb.ts
@@ -3,51 +3,51 @@ import exec from "../utils/process";
 export default class Adb {
 
     private readonly deviceId: string | null;
-    private readonly adbPath: string = 'adb';
+    private readonly adbPath: string = "adb";
 
-    constructor(device: string | null, adbPath: string = 'adb') {
+    constructor(device: string | null, adbPath: string = "adb") {
         this.deviceId = device;
         this.adbPath = adbPath;
     }
 
     public execOnDevice(command: string, ...args: string[]): Promise<string> {
-        let processArgs: string[] = [];
+        const processArgs: string[] = [];
         if (this.deviceId !== null) {
-            processArgs.push('-s', this.deviceId)
+            processArgs.push("-s", this.deviceId);
         }
         processArgs.push(command);
         processArgs.push(...args);
 
-        return exec(this.adbPath, ...processArgs)
+        return exec(this.adbPath, ...processArgs);
     }
 
     public shell(command: string, ...args: string[]): Promise<string> {
-        return this.execOnDevice('shell', command, ...args)
+        return this.execOnDevice("shell", command, ...args);
     }
 
     public pullFile(remotePath: string, localPath: string): Promise<string> {
-        return this.execOnDevice('pull', remotePath, localPath)
+        return this.execOnDevice("pull", remotePath, localPath);
     }
 
     public pushFile(localPath: string, remotePath: string, chmod: number = 0o644): Promise<string> {
-        let pushPromise = this.execOnDevice('push', localPath, remotePath);
+        const pushPromise = this.execOnDevice("push", localPath, remotePath);
 
         if (chmod) {
-            return pushPromise.then(() => this.shell('chmod', chmod.toString(), remotePath))
+            return pushPromise.then(() => this.shell("chmod", chmod.toString(), remotePath));
         }
-        return pushPromise
+        return pushPromise;
     }
 
     public getCpuAbi(): Promise<string> {
-        return this.shell('getprop', 'ro.product.cpu.abi')
+        return this.shell("getprop", "ro.product.cpu.abi");
     }
 
     public root(): Promise<string> {
-        return this.execOnDevice('root')
+        return this.execOnDevice("root");
     }
 
     public reboot(): Promise<string> {
-        return this.execOnDevice('reboot')
+        return this.execOnDevice("reboot");
     }
 
 }
