a1fb5cb0213f1687f168b9b512541645777fd634
Damien Villeneuve <contact@dvilleneuve.fr>
2019-11-29 14:05:30 +0100

    Minor refacto on index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,18 @@ import MitmModule from "./modules/mitm";
 
 const fileFetcher = new FileFetcher();
 
-commander
-    .version("0.1.0")
-    .description("Set of tools for dynamic analysis with Frida");
-
 FridaModule.prepare(fileFetcher).apply(commander);
 MitmModule.prepare(fileFetcher).apply(commander);
 ApkModule.prepare().apply(commander);
 
-commander.parse(process.argv);
+commander
+    .version("0.1.0")
+    .description("Set of tools for dynamic analysis with Frida")
+    .on("command:*", function () {
+        console.error("Invalid command: %s\n", commander.args.join(" "));
+        commander.outputHelp();
+    })
+    .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
     commander.outputHelp();
